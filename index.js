7e5bf962434da01ec604a62126d12d9d2fcd5201
nglinh <nglinh2312@gmail.com>
2015-01-25 14:27:59 +0800

    changed README

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-	var i = 0;
-	for (i = 0; i < 100; i++){
-		console.log('hohohoho');
-	}
   response.send('Hello Boostio!')
 })
 
